9e380e14755b33d9156ae7add6a7f466972d1282
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2021-01-20 18:31:45 +0100

    chore(env); update prod redict uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   spotify: {
     clientId: 'f6ebc72815c048c3acd8a4edbb8a9765',
-    redirectUri: 'http://localhost:8100/',
+    redirectUri: 'http://focused-gates-a5418c.netlify.app/',
   }
 };

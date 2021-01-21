d420f290b53708348c506db722105c50645db78f
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2021-01-21 23:10:10 +0100

    chore: update prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tracks.aqelia.com',
+  apiUrl: 'https://tracks-api.aqelia.com',
   spotify: {
     clientId: 'f6ebc72815c048c3acd8a4edbb8a9765',
     redirectUri: 'https://focused-gates-a5418c.netlify.app/',

4d895858ac7771b9fa597c300f3408989771f4d1
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2021-01-21 22:25:14 +0100

    fix: prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://giskard.aqelia.com:3000',
+  apiUrl: 'https://tracks.aqelia.com',
   spotify: {
     clientId: 'f6ebc72815c048c3acd8a4edbb8a9765',
     redirectUri: 'https://focused-gates-a5418c.netlify.app/',
